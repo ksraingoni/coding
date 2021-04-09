cfe2263b1d5d1ef8fbbbe5081dcea36f0e2c2d2b
unknown <ksraingoni07@gmail.com>
2021-04-09 06:40:29 -0700

    length of string in c

---
+++ c6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+    int a;
+    
+    char string[]="krishna pal singh";
+    a=strlen(string);
+    
+printf("%d",a);
+
+getch();
+}
