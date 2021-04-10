6fdb3fc93b1fa3f43590e3343967fb56e20e351b
unknown <ksraingoni07@gmail.com>
2021-04-10 03:44:25 -0700

    functions 1

---
+++ c7.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+ 
+void printline()
+{ 
+    int i;
+    for(i=0;i<30;i++)
+    printf("_");
+}
+ 
+
+void printline();
+void main()
+{
+    printline();
+     printf(" \n name      hello      sub    \n");
+     printline();
+     printf(" \nkrishna pal singh rajpurohit \n");
+     getch();
+
+
+ }
+
+
