b693350da3e97d2a9503718cb0e32f8e18c641d5
unknown <ksraingoni07@gmail.com>
2021-04-28 16:04:00 +0530

    ascending order of array

--- a10.cpp
+++ a10.cpp
@@ -1,22 +1,50 @@
 #include<iostream>
 using namespace std;
 
-int  main()
+int main()
 {   
-    int i,a[12],n;
-  cout<<"enter size of array"<<endl;
-   cin>>n;
+    int i,a[20],j,n,temp;
+    
+    cout<<"enter size of array:-";
+    cin>>n;
+    for(i=0;i<n;i++){
+       cout<<"enter array elements["<<i+1<<"]:";
+       cin>>a[i];
+        }
 
- for(i=0;i<n;i++)
- {
-    cin>>a[i];
- }
+    
+for(i=0;i<n;i++)
+{  
+    for(j=i+1;j<n;j++)
+       
+       if(a[j]<a[i])
+       {
+           temp=a[i];
+           a[i]=a[j];
+           a[j]=temp;
 
-  for(i=n-1;i>=0;i--)
- {
-   cout<<a[i];
- }
-  return 0;
+       }
+    
+}
+
+
+cout<<" Ascending oreder is  :";
+for(i=0;i<n;i++)
+{
+
+    cout<<"\t";
+    cout<<a[i]  ;
+}
+
+
+
+
+return 0;
+
+
+
+
+
 }
 
 
